92ca2dfc266f775f048ea5139aeb3f6d3efb72bb
Leonard D. Vorbeck <36996220+le0x99@users.noreply.github.com>
2020-02-18 17:42:37 +0100

    Update training.py

--- training.py
+++ training.py
@@ -32,10 +32,6 @@ target_vector = ['create', 'modify','tag_add',
             'tag_del', 'tag_change','loc_change',
             'new_mapper']
 
-# We dont use excessive parameter adjustment because we do not use Cross Val, therefore hardcoding the best set of parameters is pointless.
-model_params = {"VAR" : {"alpha" : [10], "tolerance" : [.2] },
-                "RF" :   {"max_depth" : [None] }
-                }
                 
 def baseline1(data, city, cols, test_size):
     dat = data[city][cols].copy()
@@ -51,7 +47,7 @@ def baseline1(data, city, cols, test_size):
     RMSE = np.sqrt(SE.mean())
     #NRMSE = RMSE/dat.std()
     res = RMSE.to_dict()
-    res["MRMSE"] = RMSE.mean()
+    #res["MRMSE"] = RMSE.mean()
     res["city"] = city
     return res
 
@@ -69,14 +65,14 @@ def baseline2(data, city,cols, test_size):
     RMSE = np.sqrt(SE.mean())
     #NRMSE = RMSE/dat.std()
     res = RMSE.to_dict()
-    res["MRMSE"] = RMSE.mean()
+    #res["MRMSE"] = RMSE.mean()
     res["city"] = city
     return res
     
-    def model_predictions(frequency, city, intercept=True):
+def model_predictions(frequency, city, intercept=True, testing_years=1):
     if frequency == "1w":
         data = data_w[city]
-        test_size = 48
+        test_size = 48 
         max_lag = 4
     elif frequency == "2w":
         data = data_2w[city]
@@ -91,13 +87,14 @@ def baseline2(data, city,cols, test_size):
         test_size = 12
         max_lag = 2
     
+    test_size = test_size*testing_years
     cols = ['create', 'modify','tag_add',
             'tag_del', 'tag_change','loc_change',
             'new_mapper']
-    #1 Obtain BL1 predictions and errors
+    #1 Obtain baseline predictions and errors
     pred_BL1 = data[-test_size-1:].shift(1).dropna()
     
-    
+    #2 Obtain MA predictions
     pred_BL2 = data[-test_size-2:].shift(1).rolling(2).mean().dropna()
     
     
@@ -140,8 +137,9 @@ def compare_predictions(variable, frequency, city):
     res = model_predictions(frequency, city)
     return pd.DataFrame({method : res[method][variable] for method in res}, index = res["True"].index)
     
-
-# Save results frequency and city wise
+    
+  - - # # Run the actual experiment # # - -  
+# City wise training and evaluation
 res_dicts = []
 for freq in ["1w", "2w", "3w", "4w"]:
     d = {}
@@ -163,88 +161,70 @@ for freq in ["1w", "2w", "3w", "4w"]:
 # Aggregate the results into a large dataframe in order to do analysis of the conditional performance of the models
 # This is a trivial task and just makes plotting easier
 def aggregate(res_w, res_2w, res_3w, res_4w):
-    city = []
-    frequency = []
-    target = []
-    error = []
-    MVE, MVE_freq, MVE_city, MVE_model = [], [], [], []
-    model = []
-    targets = ["create", "modify", "tag_add", "tag_del", "tag_change", "loc_change", "new_mapper", "MVE"]
-    
-
+    city, frequency, target, error, model = [], [], [], [], []
+    targets = ["create", "modify", "tag_add", "tag_del", "tag_change", "loc_change", "new_mapper"]
     for c in res_w:
         model_results = res_w[c]
         for var in targets:
-            if var != "MVE":
-                for i in range(len(model_results[var])):
-                    error.append(model_results[var].values[i])
-                    model.append(model_results[var].index[i])
-                    target.append(var)
-                    city.append(c)   
-                    frequency.append("1w")
-            else:
-                for i in range(len(model_results[var])):
-                    MVE.append(model_results[var].values[i])
-                    MVE_model.append(model_results[var].index[i])
-                    MVE_city.append(c)   
-                    MVE_freq.append("1w")
-
+            for i in range(len(model_results[var])):
+                error.append(model_results[var].values[i])
+                model.append(model_results[var].index[i])
+                target.append(var)
+                city.append(c)   
+                frequency.append("1w")
     for c in res_2w:
         model_results = res_2w[c]
-        for var in targets:
-            if var != "MVE":
-                for i in range(len(model_results[var])):
-                    error.append(model_results[var].values[i])
-                    model.append(model_results[var].index[i])
-                    target.append(var)
-                    city.append(c)   
-                    frequency.append("2w")
-            else:
-                for i in range(len(model_results[var])):
-                    MVE.append(model_results[var].values[i])
-                    MVE_model.append(model_results[var].index[i])
-                    MVE_city.append(c)   
-                    MVE_freq.append("2w")
-
+        for var in targets:     
+            for i in range(len(model_results[var])):
+                error.append(model_results[var].values[i])
+                model.append(model_results[var].index[i])
+                target.append(var)
+                city.append(c)   
+                frequency.append("2w")
     for c in res_3w:
         model_results = res_3w[c]
         for var in targets:
-            if var != "MVE":
-                for i in range(len(model_results[var])):
-                    error.append(model_results[var].values[i])
-                    model.append(model_results[var].index[i])
-                    target.append(var)
-                    city.append(c)   
-                    frequency.append("3w")
-            else:
-                for i in range(len(model_results[var])):
-                    MVE.append(model_results[var].values[i])
-                    MVE_model.append(model_results[var].index[i])
-                    MVE_city.append(c)   
-                    MVE_freq.append("3w")
+            for i in range(len(model_results[var])):
+                error.append(model_results[var].values[i])
+                model.append(model_results[var].index[i])
+                target.append(var)
+                city.append(c)   
+                frequency.append("3w")
     for c in res_4w:
         model_results = res_4w[c]
         for var in targets:
-            if var != "MVE":
-                for i in range(len(model_results[var])):
-                    error.append(model_results[var].values[i])
-                    model.append(model_results[var].index[i])
-                    target.append(var)
-                    city.append(c)   
-                    frequency.append("4w")
-            else:
-                for i in range(len(model_results[var])):
-                    MVE.append(model_results[var].values[i])
-                    MVE_model.append(model_results[var].index[i])
-                    MVE_city.append(c)   
-                    MVE_freq.append("4w")
-    return {"all_vars" : pd.DataFrame({"City" : city,
+            for i in range(len(model_results[var])):
+                error.append(model_results[var].values[i])
+                model.append(model_results[var].index[i])
+                target.append(var)
+                city.append(c)   
+                frequency.append("4w")
+                
+    return pd.DataFrame({"City" : city,
                          "Target" : target, "Error" :error,
-                        "Frequency" : frequency, "Model" : model}),
-           "mean" : pd.DataFrame({"City" : MVE_city, "ME" : MVE, "Frequency" : MVE_freq, "Model" : MVE_model}) }
+                        "Frequency" : frequency, "Model" : model})
   
   
-  # Restructure
+ # Save results frequency and city wise
+res_dicts = []
+for freq in ["1w", "2w", "3w", "4w"]:
+    d = {}
+    for city in cities:
+        res = model_predictions(freq, city, testing_years=2)
+        RMSE_rf = np.sqrt(((res["True"] - res["RF"])**2).mean())
+        RMSE_var = np.sqrt(((res["True"] - res["VAR"])**2).mean())
+        RMSE_bl1 = np.sqrt(((res["True"] - res["BL1"])**2).mean())
+        RMSE_bl2 = np.sqrt(((res["True"] - res["BL2"])**2).mean())
+        df = pd.DataFrame([_.to_list() for _ in [RMSE_var, RMSE_rf, RMSE_bl1, RMSE_bl2]],
+                               columns = target_vector,
+                               index=["VAR", "RF", "BL1", "BL2"])
+        df.index.name = "Model"
+        #df["MVE"] = df.mean(axis=1)
+        #df["SVE"] = df.sum(axis=1)
+        d[city] = df
+    res_dicts.append(d)
+    
+# Restructure
 model_results_w = res_dicts[0]
 model_results_2w = res_dicts[1]
 model_results_3w = res_dicts[2]
@@ -256,16 +236,9 @@ model_results_2w_norm = { city : model_results_2w[city] / model_results_2w[city]
 model_results_3w_norm = { city : model_results_3w[city] / model_results_3w[city].loc["BL1"] for city in model_results_3w  }
 model_results_m_norm = { city : model_results_m[city] / model_results_m[city].loc["BL1"] for city in model_results_m  }
 
-
-results = aggregate(model_results_w, model_results_2w, model_results_3w, model_results_m)
-results_norm = aggregate(model_results_w_norm, model_results_2w_norm, model_results_3w_norm, model_results_m_norm)
-
-
-data = results["all_vars"]
-data_mean = results["mean"]
-data_norm = results_norm["all_vars"]
-data_norm_mean = results_norm["mean"]
-
+data = aggregate(model_results_w, model_results_2w, model_results_3w, model_results_m)
+data_norm = aggregate(model_results_w_norm, model_results_2w_norm, model_results_3w_norm, model_results_m_norm) 
+ 
 #save results
 with open("results_normalized", "wb") as f:
     pickle.dump(data_norm, f)
